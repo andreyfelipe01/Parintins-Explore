793a63f8931fd5272bc04da565bac271b68456a6
andreyfelipe01 <126875712+andreyfelipe01@users.noreply.github.com>
2023-08-25 16:11:43 -0400

    Create BemVindo1.js

---
+++ src/screens/BemVindo1.js
@@ -0,0 +1,60 @@
+import {Text, View, StyleSheet,Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function Bemvindo(){
+  return(
+
+    <View style={styles.container}>
+      <LinearGradient 
+        style={styles.linearGradient}
+        colors={['#ff4142', '#c03c99', '#7b38fb']} 
+        start={{ x: 0, y: 1 }}
+        end={{ x: 1, y: 1 }}
+      />
+    <View style={styles.containerLogoText}>
+      <View style={styles.containerImage}>
+        <Image 
+        source={require('../../assets/favicon.png')}
+        style={{height:170, width:170}}
+        />
+      </View>
+      <View>
+      <Text style={styles.textWelcome}>Bem Vindo ao</Text>
+      <Text style={styles.textNameApp}>Parintins Explore</Text>
+      </View>
+    </View>
+    </View>
+  )
+}    
+const styles = StyleSheet.create({
+  linearGradient:{
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0
+    },
+    container:{
+      flex: 1,
+      justifyContent:'center',
+    },
+    containerImage:{
+      height: 200,
+      
+    },
+    containerLogoText:{
+      alignItems:'center',
+    },
+    textWelcome:{
+      color: '#FFF',
+      fontSize: 18,
+      textAlign: 'center',
+    },
+    textNameApp:{
+      color: '#FFF',
+      fontSize: 30,
+      textAlign:'center',
+      fontWeight: 'bold'
+    },
+
+  })
