be411bd2c604f830c69c4f1a59319bbce739d2d6
andreyfelipe01 <126875712+andreyfelipe01@users.noreply.github.com>
2023-08-25 16:11:08 -0400

    Update App.js
    Criação da tela bem vindo em portugues
    
    obs: ainda falta criar as dos outros idiomas

--- App.js
+++ App.js
@@ -6,17 +6,20 @@ const Pilha = createNativeStackNavigator();
 
 //Importação de telas
 import Idioma from './src/screens/Idioma';
+import Bemvindo from './src/screens/BemVindo1';
 import Welcome from './src/screens/Welcome';
 import Cadrastro from './src/screens/Novasenha';
+
 
 export default function App() {
   return (
     <NavigationContainer>
       <Pilha.Navigator>
       <Pilha.Screen name="idioma" component={Idioma} options={{headerShown: false}}/> 
+      <Pilha.Screen name='portugues'component={Bemvindo} options={{headerShown: false}} />
       <Pilha.Screen name="home" component={Welcome} options={{headerShown: false}}/>
       <Pilha.Screen name="cadastro" component={Cadrastro} options={{headerShown: false}}/>
-        
+      
       </Pilha.Navigator>
     </NavigationContainer>
   );
