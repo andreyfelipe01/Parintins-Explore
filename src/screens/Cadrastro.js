f7913982f8b38af1edcde0677a80853b87c5897d
andreyfelipe01 <126875712+andreyfelipe01@users.noreply.github.com>
2023-09-27 19:56:28 -0400

    Update Cadrastro.js

--- src/screens/Cadrastro.js
+++ src/screens/Cadrastro.js
@@ -1,60 +1,78 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet,Image } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet,Image, ScrollView, Dimensions } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import Button from '../components/Button';
+import {AntDesign} from 'react-native-vector-icons'
 
-export default function Cad(){
+export default function Cad({navigation}){
     return (
       <View style={styles.container}>
+        <LinearGradient
+          colors={['#ff4142', '#c03c99', '#7b38fb']}
+          style={styles.linearGradient}
+          start={{ x: 0, y: 1 }}
+          end={{ x: 1, y: 1 }}
+        />
+
+      <ScrollView>
+        <View style={{height: Dimensions.get("window").height}}>
+        <View style={styles.containerLogoText}>
         
-      <LinearGradient
-        colors={['#ff4142', '#c03c99', '#7b38fb']}
-        style={styles.linearGradient}
-        start={{ x: 0, y: 1 }}
-        end={{ x: 1, y: 1 }}
-      />
-
-      <View style={styles.containerLogoText}>
-      <View style={styles.containerImage}>
-          <Image
-            source={require('../../assets/favicon.png')}
-            style={{height: 170, width: 170}}
-          />
-      </View> 
-      <View>
-      <Text style={styles.textNameApp}>Inscreva-se</Text>
-          <Text style={styles.textWelcome}>Criar nova conta</Text>
+          <View style={styles.containerImage}>
+              <Image
+                source={require('../../assets/favicon.png')}
+                style={{height: 150, width: 150}}
+              />
+          </View> 
           
+          <View style={{marginBottom:'-15%'}}>
+          <Text style={styles.textNameApp}>Inscreva-se</Text>
+          <Text style={styles.textWelcome}>Criar nova conta</Text>
+              
+          </View>
+        </View>
+        
+        <View style={styles.containerTouchables}>
+          <View style={styles.form}>
+            <Text style={styles.texto}>Nome:</Text>
+            <TextInput
+              style={styles.input}
+            />
+            <Text style={styles.texto}>Nome de Usuário:</Text>
+            <TextInput
+              style={styles.input}
+             
+            />
+            <Text style={styles.texto}>Senha:</Text>
+            <TextInput
+              style={styles.input}
+              secureTextEntry={true}
+            />
+            <Text style={styles.texto}>Confirmar Senha:</Text>
+            <TextInput
+              style={styles.input}
+              secureTextEntry={true}
+            />
+             <Text style={styles.texto}>Pergunta de recuperação:</Text>
+             <TouchableOpacity onPress={()=>{navigation.navigate('ModalScreen')}}>
+              <View style={styles.openModal}>
+                <Text style={{color: '#777'}}>Selecione aqui</Text>
+               <AntDesign name='caretdown' color='#000' size={20} ></AntDesign>
+              </View>
+              </TouchableOpacity>
+            <TouchableOpacity style={styles.button}
+            >
+              <Button text="Inscrever-se" />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text style={styles.follow}>Continuar sem conta </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        </View>
+        </ScrollView>
       </View>
-      </View>
-      <View style={styles.containerTouchables}>
-
-      <View>
-        <Text>Email:</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType="email-address"
-        />
-
-        <Text>Senha:</Text>
-        <TextInput
-          style={styles.input}
-          secureTextEntry={true}
-        />
-
-        <Text>Confirmar Senha:</Text>
-        <TextInput
-          style={styles.input}
-          secureTextEntry={true}
-        />
-
-        <TouchableOpacity
-          style={styles.botao}
-        >
-          <Text style={styles.botaoTexto}>Inscrever-se</Text>
-        </TouchableOpacity>
-      </View>
-      </View>
-      </View>
+     
       
     );
   }
@@ -67,22 +85,31 @@ const styles = StyleSheet.create({
     right: 0
   },
   container:{
+
     height: '100%'
   },
   containerLogoText:{
-    height: '50%',
+    height: '35%',
+    marginTop:'-9%',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   containerTouchables:{
-    height: '50%',
-    alignItems: 'center',
-    justifyContent: 'flex-end'
+    height: '5%',
+    alignItems: 'center'
+  },
+  form:{
+    height: 500,
+    width:350,
+    padding:20,
+    borderRadius:8,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
   },
   containerImage:{
-    height: 200,
-    alignItems: 'center',
-    justifyContent: 'center'
+    height: '50%',
+    alignItems: 'center', 
+    justifyContent: 'center',
+
   },
   textWelcome:{
     color: '#FFF',
@@ -95,35 +122,40 @@ const styles = StyleSheet.create({
     textAlign:'center',
     fontWeight: 'bold'
   },
+
+  texto:{
+    color:'white',
+    padding:9
+  },
   input: {
-    width: 250,
     height: 40,
     borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 10,
-    paddingHorizontal: 10,
+    borderRadius: 50,
+    backgroundColor: '#fff',
+    paddingLeft:14
   },
-  botao: {
-    backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 5,
+  button:{
+    alignItems: 'center',
+    paddingTop: 4,
+    marginTop:'6%'
   },
-  botaoTexto: {
-    color: 'white',
-    fontSize: 16,
+  follow:{
+    textAlign: 'center',
+    fontSize: 14,
+    paddingTop: 5,
+    color: '#fff',
+    fontWeight: 'bold',
+    
   },
-  form:{
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    borderTopRightRadius: 200,
-    borderTopLeftRadius: 200,
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
-    height: '100%',
-    transform: [{scaleX:1.5}],
-    overflow: 'hidden',
+  openModal:{
+    backgroundColor:'white',
+    borderRadius:50,
+    padding:4,
+    paddingLeft:14,
+    height: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   },
+
 });
